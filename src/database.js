840fc605bf59a0606274a90630a823e84000fff0
Maxi <emanuel-salas@hotmail.com>
2022-09-03 02:09:15 -0300

    aja

--- src/database.js
+++ src/database.js
@@ -2,22 +2,31 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
+/* const {
   DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_NAME
-} = process.env;
+} = process.env; */
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_NAME}?ssl=no-verify`, {
+/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_NAME}?ssl=no-verify`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+ */
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true //false
+  });
+}
 
-try {
+/* try {
   sequelize.authenticate();
   console.log('Conexion exitosa a la base de datos');
 } catch (error) {
   console.error('no es posible conectarse a la base de datos:', error);
-}
+} */
 const basename = path.basename(__filename);
 
 
